a8f53bf9a35e80e8bf0577e23e4f9f156b519871
Haider Mahmood <haider.slsc@gmail.com>
2024-10-22 19:09:00 +0500

    created db connection with mongodb

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const connectDB = async () => {
     const productSchema = new mongoose.Schema({});
     const product = mongoose.model('product', productSchema);
     const data = await product.find();
-
-    console.warn(data);
 }
 
 connectDB();
