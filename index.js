e2cb67dac91f3eff15c16cbf5dac922fb3fae235
Haider Mahmood <haider.slsc@gmail.com>
2024-10-23 05:58:29 +0500

    Created API for login user

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.use(cors());
 app.post('/register', async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     resp.send(result);
+});
+
+app.post('/login', async (req, resp) => {
+    if (req.body.email && req.body.password) {
+        const user = await User.findOne(req.body).select('-password');
+        if (user) resp.send(user)
+        else resp.send({ result: 'No user found' })
+    } else {
+        resp.send({ result: 'No user found' })
+    }
 });
 
 app.listen(5000);
