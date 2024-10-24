542ed187f0f00a900ccf2a9dbd14cad0d682d92a
Haider Mahmood <haider.slsc@gmail.com>
2024-10-24 08:50:08 +0500

    Created Update APIs (#8)
    * Created one API for fetching single product
    * Created other API to update the product

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.post('/register', async (req, resp) => {
 app.post('/login', async (req, resp) => {
     if (req.body.email && req.body.password) {
         const user = await User.findOne(req.body).select('-password');
-        if (user) resp.send(user)
-        else resp.send({ result: 'No user found' })
+        if (user) resp.send(user);
+        else resp.send({ result: 'No user found' });
     } else {
         resp.send({ result: 'No user found' });
     }
@@ -41,6 +41,17 @@ app.get('/products', async (req, resp) => {
 
 app.delete('/product/:id', async (req, resp) => {
     const result = await Product.deleteOne({ _id: req.params.id });
+    resp.send(result);
+});
+
+app.get('/product/:id', async (req, resp) => {
+    const result = await Product.findOne({ _id: req.params.id });
+    if (result) resp.send(result);
+    else resp.send({ result: 'No record found' });
+});
+
+app.put('/product/:id', async (req, resp) => {
+    let result = await Product.updateOne({ _id: req.params.id }, { $set: req.body });
     resp.send(result);
 });
 
