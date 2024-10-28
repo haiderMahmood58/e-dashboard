182a868abf9f6d7fc13d0a8ae08dab3a9120f676
Haider Mahmood <haider.slsc@gmail.com>
2024-10-29 00:04:21 +0500

    Created API for searching products (#9)

--- index.js
+++ index.js
@@ -55,4 +55,15 @@ app.put('/product/:id', async (req, resp) => {
     resp.send(result);
 });
 
+app.get('/search/:key', async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } }
+        ]
+    });
+    resp.send(result);
+});
+
 app.listen(5000);
